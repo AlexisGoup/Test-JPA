aec37a9a49939decd4dfc15742392bca08be8838
Alexis <alexis.goupillot.dev@gmail.com>
2021-02-25 15:08:11 +0100

    TP5

--- src/banque/Application.java
+++ src/banque/Application.java
@@ -10,9 +10,11 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 import banque.entities.Adresse;
+import banque.entities.AssuranceVie;
 import banque.entities.Banque;
 import banque.entities.Client;
 import banque.entities.Compte;
+import banque.entities.LivretA;
 import banque.entities.Operation;
 
 public class Application {
@@ -31,41 +33,38 @@ public class Application {
 	}
 	
 	private static Client instAll() {
-		// BNP 
+				// BNP 
 				Banque bnp = new Banque();
 				bnp.setNom("BNP Paribas");
 				
 				// CLIENT 1
 				Client client1 = new Client();
+				
+				// ANNIVERSAIRE DU CLIENT 1
 				LocalDate birthdayClient1 = LocalDate.now();
-				Adresse adresseClient1 = new Adresse();
-				Compte compte_1DuClient1 = new Compte();
-				Compte compte_2DuClient1 = new Compte();
-				Operation virementDebase = new Operation();
-				Operation virementDebase2 = new Operation();
 				
 				// CONSTRUCTION ADRESSE CLIENT 1
+				Adresse adresseClient1 = new Adresse();
 				adresseClient1.setCodePostal(34000);
 				adresseClient1.setNumero(6);
 				adresseClient1.setRue("Rue du Puech");
 				adresseClient1.setVille("Montpellier");
 				
 				// CONSTRUCTION DE DEUX COMPTES POUR CLIENT 1
+				Compte compte_1DuClient1 = new Compte();
 				compte_1DuClient1.setNumero("55");
-				compte_1DuClient1.setSolde(1000000);		
+				compte_1DuClient1.setSolde(1000000);
+				Compte compte_2DuClient1 = new Compte();
 				compte_2DuClient1.setNumero("56");
 				compte_2DuClient1.setSolde(5000);
 				
-				// AJOUT SUR CLIENT 1 DE DEUX COMPTES 
-				List<Compte> listeComptes_Client1 = client1.getComptes();
-				listeComptes_Client1.add(compte_1DuClient1);
-				listeComptes_Client1.add(compte_2DuClient1);
-				
 				// CREATION DE DEUX VIREMENTS
+				Operation virementDebase = new Operation();
 				virementDebase.setCompte(compte_1DuClient1);
 				virementDebase.setDate(LocalDateTime.now());
 				virementDebase.setMontant(4500);
-				virementDebase.setMotif("Anniversaire");		
+				virementDebase.setMotif("Anniversaire");
+				Operation virementDebase2 = new Operation();
 				virementDebase2.setCompte(compte_1DuClient1);
 				virementDebase2.setDate(LocalDateTime.now());
 				virementDebase2.setMontant(4500);
@@ -75,6 +74,26 @@ public class Application {
 				List<Operation> listeOperations_Client1 = compte_1DuClient1.getOperations();
 				listeOperations_Client1.add(virementDebase);
 				listeOperations_Client1.add(virementDebase2);
+				
+				// CREATION D'UN LIVRET A
+				LivretA livretA_Client1 = new LivretA();
+				livretA_Client1.setNumero("152");
+				livretA_Client1.setSolde(1500);
+				livretA_Client1.setTaux(1.5);
+				
+				// CREATION D'UNE ASSURANCE VIE
+				AssuranceVie assVie_Client1 = new AssuranceVie();
+				assVie_Client1.setDateFin(birthdayClient1);
+				assVie_Client1.setNumero("153");
+				assVie_Client1.setSolde(6500);
+				assVie_Client1.setTaux(3.5);
+				
+				// AJOUT SUR CLIENT 1 DE DEUX COMPTES + LIVRET A + ASSURANCE VIE
+				List<Compte> listeComptes_Client1 = client1.getComptes();
+				listeComptes_Client1.add(compte_1DuClient1);
+				listeComptes_Client1.add(compte_2DuClient1);
+				listeComptes_Client1.add(livretA_Client1);
+				listeComptes_Client1.add(assVie_Client1);
 				
 				// CONSTRUCTION DES ATTRIBUTS DE CLIENT 1
 				client1.setNom("Bouiffe");
--- src/banque/entities/Compte.java
+++ src/banque/entities/Compte.java
@@ -8,12 +8,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
 @Entity
+@Inheritance(strategy=InheritanceType.JOINED)
 public class Compte {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
